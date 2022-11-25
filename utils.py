43757b8ec5cd7d31f88f596f769632f23dfc1c42
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-25 16:25:40 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -220,18 +220,26 @@ async def is_droplink_url(url):
     return domain
 
 
-async def broadcast_admins(c: Client, Message, sender=False):
-    admins = ADMINS[:]
-    
-    if sender:
-        admins.remove(sender)
-
-    for i in admins:
-        try:
-            await c.send_message(i, Message)
-        except PeerIdInvalid:
-            logging.info(f"{i} have not yet started the bot")
-    return
+async def broadcast_messages(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(user_id, message)
+    except InputUserDeactivated:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
 
 async def get_size(size):
     """Get size in readable format"""
