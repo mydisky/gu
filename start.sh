bac2fe2b6635b8ec276348dab95c6434916e7c50
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-25 16:43:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/YourUsername/ATG-Shortener-V2.git /ATG-Shortener-V2
+  git clone https://github.com/maullikpatell/DTG-linkszad-bot.git /DTG-linkszad-bot
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /ATG-Shortener-V2
+  git clone $SOURCE_CODE /DTG-linkszad-bot
 fi
-cd /ATG-Shortener-V2
+cd /DTG-linkszad-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
+
