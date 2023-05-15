5ae34e1893cbea7912e83c4a0217296fb95949a8
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-05-15 23:59:14 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -1,10 +1,21 @@
 from aiohttp import web
+import datetime
+from helpers import temp
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("LazyDeveloper")
+     runtime = datetime.datetime.now()
+    t = runtime - temp.START_TIME
+    runtime = str(datetime.timedelta(seconds=t.seconds))
+
+    res = {
+        "status": "running",
+        "bot": temp.BOT_USERNAME,
+        "runtime": runtime,
+    }
+    return web.json_response(res)
 
 
 async def web_server():
