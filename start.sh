dc2a4faef8c65ccdee2783f0e10361cec2dfe02a
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-05-15 20:56:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
-if [ -z $SOURCE_CODE ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/maullikpatell/DTG-linkszad-bot.git /DTG-linkszad-bot
-else
-  echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /DTG-linkszad-bot
-fi
-cd /DTG-linkszad-bot
-pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 -m main
+python3 main.py
 
