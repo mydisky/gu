3726bb3357e0c1d316b0d0b55c5118e9a526aee2
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-05-16 00:30:52 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -6,14 +6,8 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-     runtime = datetime.datetime.now()
-    t = runtime - temp.START_TIME
-    runtime = str(datetime.timedelta(seconds=t.seconds))
-
     res = {
         "status": "running",
-        "bot": temp.BOT_USERNAME,
-        "runtime": runtime,
     }
     return web.json_response(res)
 
