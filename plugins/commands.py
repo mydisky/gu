82898e9b77679548840eb4553694781fc607551e
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-05-16 00:14:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ from utils import extract_link, get_me_button, get_size, getHerokuDetails
 logger = logging.getLogger(__name__)
 
 user_commands = ["mdisk_api", "shortener_api", "header", "footer", "username", "banner_image", "me"]
-avl_web = ["dtglinks.in",]
+avl_web = ["nanolinks.in",]
 
 avl_web1 = "".join(f"- {i}\n" for i in avl_web)
 
@@ -51,7 +51,7 @@ async def help_command(c, m: Message):
                 firstname=temp.FIRST_NAME,
                 username=temp.BOT_USERNAME,
                 repo=SOURCE_CODE,
-                owner="@ask_admin001" )
+                owner="@tgnvs" )
 
     if WELCOME_IMAGE:
         return await m.reply_photo(photo=WELCOME_IMAGE, caption=s, reply_markup=HELP_REPLY_MARKUP)
@@ -245,6 +245,23 @@ async def banner_image_handler(bot, m: Message):
                 return await m.reply_text("Image URL is Invalid")
 
 
+
+@Client.on_message(filters.command("base_site") & filters.private)
+@private_use
+async def base_site_handler(bot, m: Message):
+    user_id = m.from_user.id
+    user = await get_user(user_id)
+    cmd = m.command
+    site = user["base_site"]
+    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to droplink.co"
+    if len(cmd) == 1:
+        return await m.reply(text=text, disable_web_page_preview=True)
+    elif len(cmd) == 2:
+        base_site = cmd[1].strip()
+        if not domain(base_site):
+            return await m.reply(text=text, disable_web_page_preview=True)
+        await update_user_info(user_id, {"base_site": base_site})
+        await m.reply("Base Site updated successfully")
 
 
 @Client.on_message(filters.command('me') & filters.private)
